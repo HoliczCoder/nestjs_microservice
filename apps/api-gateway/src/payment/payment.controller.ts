b3f691029cd2a04be7fb9993927375554a33cee1
MinhNoob <minhtranconglis@gmail.com>
2023-05-31 13:42:25 +0700

    complete

--- apps/api-gateway/src/payment/payment.controller.ts
+++ apps/api-gateway/src/payment/payment.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, ValidationPipe, Body } from '@nestjs/common';
+import { Controller, ValidationPipe, Body, Post } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { MakePaymentDto } from '@nestjs-microservices/shared/dto';
 
-@Controller('payment')
+@Controller('payments')
 export class PaymentController {
   constructor(private readonly paymentService: PaymentService) {}
 
+  @Post('pay')
   makePayment(@Body(ValidationPipe) makePaymentDto: MakePaymentDto) {
     return this.paymentService.makePayment(makePaymentDto);
   }
